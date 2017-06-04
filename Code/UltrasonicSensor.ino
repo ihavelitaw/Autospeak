6853de90d644e0d94f5ed31ba2b3cf10168bc2d7
dmerkouris <dimmerko@gmail.com>
2017-06-04 18:27:43 +0300

    Update UltrasonicSensor.ino

--- Code/UltrasonicSensor.ino
+++ Code/UltrasonicSensor.ino
@@ -1,13 +1,8 @@
 /*
 HC-SR04 Ping distance sensor]
 VCC to arduino 5v GND to arduino GND
-Echo to Arduino pin 13 Trig to Arduino pin 12
-Red POS to Arduino pin 11
-Green POS to Arduino pin 10
-560 ohm resistor to both LED NEG and GRD power rail
-More info at: http://goo.gl/kJ8Gl
+Echo to Arduino pin 13 Trig to Arduino pin
 Original code improvements to the Ping sketch sourced from Trollmaker.com
-Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
 */
 
 #define trigPin 13
