0a94a9c65ba2578f685e5ba02a5f7fad50118c67
dmerkouris <dimmerko@gmail.com>
2017-06-15 01:16:01 +0300

    Create System.ino

--- Code/System.ino
+++ Code/System.ino
@@ -120,24 +120,4 @@ void loop(){
          }         */
          takeLowTime = true;
        }
-
-   /*  if(digitalRead(pirPin) == LOW){       
-       digitalWrite(LED_BUILTIN, LOW);  //the led visualizes the sensors output pin state
-
-       if(takeLowTime){
-        lowIn = millis();          //save the time of the transition from high to LOW
-        takeLowTime = false;       //make sure this is only done at the start of a LOW phase
-        }
-       //if the sensor is low for more than the given pause, 
-       //we assume that no more motion is going to happen
-       if(!lockLow && millis() - lowIn > pause){  
-           //makes sure this block of code is only executed again after 
-           //a new motion sequence has been detected
-           lockLow = true;                        
-           Serial.print("motion ended at ");      //output
-           Serial.print((millis() - pause)/1000);
-           Serial.println(" sec");
-           delay(50);
-           }
-       }        */
   }
