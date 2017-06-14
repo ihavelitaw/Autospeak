b02468379aa594d12e1b950ebe825dcd3abc6ee5
dmerkouris <dimmerko@gmail.com>
2017-06-14 19:42:28 +0300

    Create System.ino

---
+++ Code/System.ino
@@ -0,0 +1,142 @@
+//VARS
+//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 30;        
+
+//the time when the sensor outputs a low impulse
+long unsigned int lowIn;         
+
+//the amount of milliseconds the sensor has to be low 
+//before we assume all motion has stopped
+long unsigned int pause = 5000;  
+
+boolean lockLow = true;
+boolean takeLowTime;  
+int  pirPin = 2;   //the digital pin connected to the PIR sensor's output
+// This code is for the builtin led. Using the PIR the builtin led flashes
+
+#define echoPin 10 // Echo Pin
+#define trigPin 11 // Trigger Pin
+
+int maximumRange = 1000; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
+/////////////////////////////----------------------------------/////////////////////////////////////////////------------------------------------------
+
+#include "SoftwareSerial.h"
+SoftwareSerial mySerial(10, 11);
+# define Start_Byte 0x7E
+# define Version_Byte 0xFF
+# define Command_Length 0x06
+# define End_Byte 0xEF
+# define Acknowledge 0x00 //Returns info with command 0x41 [0x01: info, 0x00: no info]
+
+# define ACTIVATED LOW
+
+int buttonNext = 2;
+int buttonPause = 3;
+int buttonPrevious = 4;
+boolean isPlaying = false;
+
+
+///////////////////////////--------------------------------------///////////////////////////////////////////--------------------------------------------
+//SETUP
+void setup(){
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(pirPin, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+      }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
+    
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LED_BUILTIN, OUTPUT); // Use LED indicator (if required)
+  pinMode(buttonPause, INPUT);
+digitalWrite(buttonPause,HIGH);
+pinMode(buttonNext, INPUT);
+digitalWrite(buttonNext,HIGH);
+pinMode(buttonPrevious, INPUT);
+digitalWrite(buttonPrevious,HIGH);
+
+mySerial.begin (9600);
+delay(1000);
+playFirst();
+isPlaying = true;
+  }
+
+////////////////////////////
+//LOOP
+void loop(){
+
+     if(digitalRead(pirPin) == HIGH){
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////-------------------------------
+       digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance <= maximumRange || distance >= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ digitalWrite(LED_BUILTIN, HIGH); 
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ digitalWrite(LED_BUILTIN, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
+
+ ////////////////////////////////////////////////
+     /*  if(lockLow){  
+         //makes sure we wait for a transition to LOW before any further output is made:
+         lockLow = false;            
+         Serial.println("---");
+         Serial.print("motion detected at ");
+         Serial.print(millis()/1000);
+         Serial.println(" sec"); 
+         delay(50);
+         }         */
+         takeLowTime = true;
+       }
+
+   /*  if(digitalRead(pirPin) == LOW){       
+       digitalWrite(LED_BUILTIN, LOW);  //the led visualizes the sensors output pin state
+
+       if(takeLowTime){
+        lowIn = millis();          //save the time of the transition from high to LOW
+        takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+        }
+       //if the sensor is low for more than the given pause, 
+       //we assume that no more motion is going to happen
+       if(!lockLow && millis() - lowIn > pause){  
+           //makes sure this block of code is only executed again after 
+           //a new motion sequence has been detected
+           lockLow = true;                        
+           Serial.print("motion ended at ");      //output
+           Serial.print((millis() - pause)/1000);
+           Serial.println(" sec");
+           delay(50);
+           }
+       }        */
+  }
